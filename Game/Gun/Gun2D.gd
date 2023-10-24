028493f211128ac7fd02d710735b60a828df64f9
R9_ <neamtrares@naver.com>
2023-10-24 20:29:49 +0300

    No smear frame

--- Game/Gun/Gun2D.gd
+++ Game/Gun/Gun2D.gd
@@ -1,13 +1,16 @@
 extends Node2D
 
 func _process(delta):
-	if $AnimatedSprite2D.is_playing():
+	if $AnimatedSprite2D.is_playing() and $AnimatedSprite2D.animation == "shoot":
 		Global.can_shoot = false
 	else:
 		Global.can_shoot = true
 	if Global.shooting:
+		$AnimatedSprite2D.stop()
 		$AnimatedSprite2D.play("shoot")
 		Global.can_shoot = false
+	if Global.can_shoot and not Global.shooting:
+		$AnimatedSprite2D.play("default")
 
 #func _input(event):
 #	if event.is_action_pressed("shoot") and not Global.shooting and Global.can_shoot:
